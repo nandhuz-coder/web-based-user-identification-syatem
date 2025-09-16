7493ec749bd5adcdd938529daad3d6dacaf51aac
Nandha Gopan <nandhagopan102@gmail.com>
2025-09-16 12:48:26 +0530

    Added user login landed page.

--- routes/middleware/redirect.js
+++ routes/middleware/redirect.js
@@ -5,7 +5,7 @@ function roleRedirect(req, res, next) {
     if (req.user.role === 'admin') {
         return res.redirect('/admin/dashboard');
     } else {
-        return res.redirect('/user/dashboard');
+        return res.redirect('/users/');
     }
 }
 
